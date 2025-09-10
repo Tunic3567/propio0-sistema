649c0f8c9605cdb1bc3273694ba561ef007faf9d
Tunic3567 <brayanramirez356@gmail.com>
2025-09-10 11:31:24 -0500

    Configurar SPA con hash routing para hosting gratuito

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import AdminDashboard from '../views/AdminDashboard.vue'
 import VendedorDashboard from '../views/VendedorDashboard.vue'
@@ -41,7 +41,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
